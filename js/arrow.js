de41e6b6c0ad660dfe5e2d1f79c1fb360c5f388d
ianellison <ianellison87@yahoo.com>
2019-07-25 08:24:05 -0700

    can shoot arrows and they stick

--- js/arrow.js
+++ js/arrow.js
@@ -1,92 +1,92 @@
-// // array of all arrows
-const arrows = [];
+// // // array of all arrows
+// const arrows = [];
 
-// adjusts arrow speed
-const speedMod = 4;
+// // adjusts arrow speed
+// const speedMod = 4;
 
-const addArrow = function () {
-  arrows.unshift(new Arrow()); // unshift adds to FRONT of arrows array
-  currArrow = arrows[0];
-}
+// const addArrow = function () {
+//   arrows.unshift(new Arrow()); // unshift adds to FRONT of arrows array
+//   currArrow = arrows[0];
+// }
 
-// Arrow prototype
-function Arrow() {
-  this.x = shootingCirc.x;
-  this.y = shootingCirc.y;
-  this.arrowTipCoords = {
-    x: this.x + 20,
-    y: this.y
-  };
-  // left and right parts of the arrow head
-  this.leftTipCoords = {
-    x: this.x + 17,
-    y: this.y - 3
-  }
-  this.rightTipCoords = {
-    x: this.x + 17,
-    y: this.y + 3
-  }
-  this.velX = 0;
-  this.velY = 0;
-  this.speed = 0;
-  this.firing = false;
-}
-Arrow.prototype.fireArrow = function () {
-  if (mousePos && !this.firing) {
-    this.speed = Math.min(shootingCirc.r,
-      distBetween(shootingCirc, mousePos)) / speedMod;
-    this.velX = Math.cos(angleBetween(mousePos, shootingCirc)) * this.speed;
-    this.velY = Math.sin(angleBetween(mousePos, shootingCirc)) * this.speed;
-    this.firing = true;
-    addArrow();
-  }
-}
-Arrow.prototype.calcTrajectory = function () {
-  if (this.y <= groundPoint && this.firing) {
-    this.velY += gravity;
-    this.x += this.velX;
-    this.y += this.velY;
-  } else {
-    this.velX = 0;
-    this.velY = 0;
-    this.firing = false;
-  }
-};
-Arrow.prototype.calcArrowHead = function () {
-  if (this.firing) {
-    var angle = Math.atan2(this.velX, this.velY);
-  } else if (mousePos && this == currArrow) {
-    var angle = Math.PI / 2 - angleBetween(mousePos, shootingCirc);
-  } else return;
+// // Arrow prototype
+// function Arrow() {
+//   this.x = shootingCirc.x;
+//   this.y = shootingCirc.y;
+//   this.arrowTipCoords = {
+//     x: this.x + 20,
+//     y: this.y
+//   };
+//   // left and right parts of the arrow head
+//   this.leftTipCoords = {
+//     x: this.x + 17,
+//     y: this.y - 3
+//   }
+//   this.rightTipCoords = {
+//     x: this.x + 17,
+//     y: this.y + 3
+//   }
+//   this.velX = 0;
+//   this.velY = 0;
+//   this.speed = 0;
+//   this.firing = false;
+// }
+// Arrow.prototype.fireArrow = function () {
+//   if (mousePos && !this.firing) {
+//     this.speed = Math.min(shootingCirc.r,
+//       distBetween(shootingCirc, mousePos)) / speedMod;
+//     this.velX = Math.cos(angleBetween(mousePos, shootingCirc)) * this.speed;
+//     this.velY = Math.sin(angleBetween(mousePos, shootingCirc)) * this.speed;
+//     this.firing = true;
+//     addArrow();
+//   }
+// }
+// Arrow.prototype.calcTrajectory = function () {
+//   if (this.y <= groundPoint && this.firing) {
+//     this.velY += gravity;
+//     this.x += this.velX;
+//     this.y += this.velY;
+//   } else {
+//     this.velX = 0;
+//     this.velY = 0;
+//     this.firing = false;
+//   }
+// };
+// Arrow.prototype.calcArrowHead = function () {
+//   if (this.firing) {
+//     var angle = Math.atan2(this.velX, this.velY);
+//   } else if (mousePos && this == currArrow) {
+//     var angle = Math.PI / 2 - angleBetween(mousePos, shootingCirc);
+//   } else return;
 
-  this.arrowTipCoords.x = this.x + 20 * Math.sin(angle);
-  this.arrowTipCoords.y = this.y + 20 * Math.cos(angle);
-  var arrowTip = { x: this.arrowTipCoords.x, y: this.arrowTipCoords.y }
+//   this.arrowTipCoords.x = this.x + 20 * Math.sin(angle);
+//   this.arrowTipCoords.y = this.y + 20 * Math.cos(angle);
+//   var arrowTip = { x: this.arrowTipCoords.x, y: this.arrowTipCoords.y }
 
-  this.leftTipCoords.x = arrowTip.x - 3 * Math.sin(angle - Math.PI / 4);
-  this.leftTipCoords.y = arrowTip.y - 3 * Math.cos(angle - Math.PI / 4);
-  this.rightTipCoords.x = arrowTip.x - 3 * Math.sin(angle + Math.PI / 4);
-  this.rightTipCoords.y = arrowTip.y - 3 * Math.cos(angle + Math.PI / 4);
-};
-Arrow.prototype.drawArrow = function () {
-  this.calcTrajectory();
-  this.calcArrowHead();
-  var arrowTip = this.arrowTipCoords;
-  var leftTip = this.leftTipCoords;
-  var rightTip = this.rightTipCoords;
+//   this.leftTipCoords.x = arrowTip.x - 3 * Math.sin(angle - Math.PI / 4);
+//   this.leftTipCoords.y = arrowTip.y - 3 * Math.cos(angle - Math.PI / 4);
+//   this.rightTipCoords.x = arrowTip.x - 3 * Math.sin(angle + Math.PI / 4);
+//   this.rightTipCoords.y = arrowTip.y - 3 * Math.cos(angle + Math.PI / 4);
+// };
+// Arrow.prototype.drawArrow = function () {
+//   this.calcTrajectory();
+//   this.calcArrowHead();
+//   var arrowTip = this.arrowTipCoords;
+//   var leftTip = this.leftTipCoords;
+//   var rightTip = this.rightTipCoords;
 
-  ctx.beginPath();
-  ctx.moveTo(this.x, this.y);
-  ctx.lineTo(arrowTip.x, arrowTip.y);
+//   ctx.beginPath();
+//   ctx.moveTo(this.x, this.y);
+//   ctx.lineTo(arrowTip.x, arrowTip.y);
 
-  ctx.moveTo(arrowTip.x, arrowTip.y);
-  ctx.lineTo(leftTip.x, leftTip.y);
+//   ctx.moveTo(arrowTip.x, arrowTip.y);
+//   ctx.lineTo(leftTip.x, leftTip.y);
 
-  ctx.moveTo(arrowTip.x, arrowTip.y);
-  ctx.lineTo(rightTip.x, rightTip.y);
+//   ctx.moveTo(arrowTip.x, arrowTip.y);
+//   ctx.lineTo(rightTip.x, rightTip.y);
 
-  ctx.strokeStyle = "black";
-  ctx.stroke();
-};
+//   ctx.strokeStyle = "black";
+//   ctx.stroke();
+// };
 
-module.exports = { addArrow, arrows, Arrow};
+// module.exports = { addArrow, arrows, Arrow};
